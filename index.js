5985012400fae6019267f75b7584f2d7f9d12640
juanefrain89 <302420141@ute.edu.mx>
2024-11-28 02:11:21 -0600

    Js2

---
+++ index.js
@@ -0,0 +1,20 @@
+// server-prod.js (Servidor de Producción)
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);  // Inicializar Socket.IO con el servidor HTTP
+
+// Endpoint para cuando se detecte un cambio en producción
+app.post('/update', (req, res) => {
+    // Emitir un evento a todos los clientes conectados
+    io.emit('update-notification', { message: 'El código ha sido actualizado en producción!' });
+    res.send('Actualización enviada a los clientes.');
+});
+
+// Escuchar en el puerto 3000
+server.listen(3000, () => {
+    console.log('Servidor de producción escuchando en el puerto 3000');
+});
